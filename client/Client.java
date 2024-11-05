c9a7e93cd75f4ad279b10514affc83ca209ab3c3
Jinha Kim <143933539+rhymer3431@users.noreply.github.com>
2024-11-05 21:10:31 +0900

    Update Client.java
    add comments

--- client/Client.java
+++ client/Client.java
@@ -4,38 +4,39 @@ import java.net.*;
 import java.util.*;
 
 public class Client {
-
     private static String server_ip;
     private static int port;
     private static ClientCommandHandler command;
-    private static BufferedReader keyboard;
-    private static BufferedReader clientReader;
-    private static PrintWriter clientWriter;
 
     private static Socket client;
 
-
     public static void main(String[] args) throws IOException{
-        getServerInfo();
-        connect();
-        start();
-    
+        // read server config information
+        getServerInfo(); 
+        
+        // intialize socket, reader, and writer connected to server
+        connect(); 
 
+        // start quiz
+        start();
     }
 
     public static void connect(){
         try{
+            // initialize Socket
             client = new Socket(server_ip,port);
 
-            keyboard = new BufferedReader(
+            // initialize reader and writer
+            BufferedReader keyboard = new BufferedReader( // for input by user 
                 new InputStreamReader(System.in)
             );
-            clientReader = new BufferedReader(
+            BufferedReader clientReader = new BufferedReader(
                 new InputStreamReader(client.getInputStream())
             );
-            clientWriter = new PrintWriter(client.getOutputStream(),true);
+            PrintWriter clientWriter = new PrintWriter(client.getOutputStream(),true);
 
-            command = new ClientCommandHandler(clientReader,clientWriter);
+            // ClientHandler: Class that defines various command to communicate server  
+            command = new ClientCommandHandler(keyboard,clientReader,clientWriter);
             System.out.printf("connected\n\n");
             
         }
@@ -48,32 +49,35 @@ public class Client {
 
         System.out.printf("get server information..\n");
 
+        // open info file, read line by line and separate according to parameters.
         try(Scanner reader = new Scanner(new File("./client/server_info.dat"))){
             while(reader.hasNextLine()){
+                int NAME = 0;
+                int CONTENT = 1;
                 String line = reader.nextLine();
-                String[] configs = line.split(":");
+                String[] configs = line.split(":"); // format like name:content
 
-                switch(configs[0]){
-                    case "server ip": server_ip = configs[1]; break;
-                    case "port number": port = Integer.parseInt(configs[1]); break;
+                switch(configs[NAME]){ // allocate each data
+                    case "server ip": server_ip = configs[CONTENT]; break;
+                    case "port number": port = Integer.parseInt(configs[CONTENT]); break;
                 }
                 
 
             }
             
+            // print received data
             System.out.printf("---------------------\n");
             System.out.printf("ip: %s\n",server_ip);
             System.out.printf("port: %d\n",port);
             System.out.printf("---------------------\n\n");
         }
-        catch(Exception e){
+        catch(Exception e){ // if server_info not founded,
             // default setting
             server_ip = "127.0.0.1";
             port = 1234;
-            e.printStackTrace();
         }
     }
-        
+
     
     public static void start() throws IOException{
         System.out.printf("\n*******************************\n");
@@ -82,26 +86,22 @@ public class Client {
         System.out.printf("*                             *\n");
         System.out.printf("*******************************\n\n");
 
-        // request quiz
+        // a quiz of 10 questions
         try {
             for (int i=0; i<10; i++){
-                // request question to server
-                String question = command.getQuestion(i);
-                
-                System.out.printf("[Quiz %d/10]\n%s\n",i+1,question);
-                System.out.printf("\nEnter answer : ");
-                String answer = keyboard.readLine();
-
-                command.getResult(i,answer);
-                
+                command.question(i);
+                command.answer(i);
                 System.out.println();
                 System.out.println();
                 
             }
+
+            // get total score from server
             int score = command.getScore(); 
             System.out.printf("\nyour total score is %d!\n",score);
 
-            clientWriter.println("disconnect");
+            // alert disconnection to server
+            command.disconnect();
             
         } catch (Exception e) {
             e.printStackTrace();
