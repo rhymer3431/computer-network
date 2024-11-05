0868906f18bdfbbceae2981100dc2f6313517135
Jinha Kim <143933539+rhymer3431@users.noreply.github.com>
2024-11-05 21:13:20 +0900

    Update command

--- server/Server.java
+++ server/Server.java
@@ -1,10 +1,20 @@
 package server;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Server {
     private static ServerSocket serverSocket;
+    public static ArrayList <String[]> QAList;
     static final int port = 12345;
+
+    public static void main(String[] args){
+        QAList = getQuizAnswer();
+        new Server();
+
+    }
+
     public Server(){
         try {
             System.out.printf("trying to open server..\n");
@@ -14,7 +24,7 @@ public class Server {
             // server is always opened
             while(true){
                 Socket newClient = serverSocket.accept();
-                new ClientHandler(newClient).start();
+                new ClientHandler(newClient,QAList).start();
             }
 
         } catch (IOException e) {
@@ -22,18 +32,22 @@ public class Server {
         } 
     }
 
-    public static void main(String[] args){
-        new Server();
+    public static ArrayList <String[]> getQuizAnswer(){
+        ArrayList<String[]> QAList = new ArrayList<>();
+        try(Scanner reader = new Scanner(new File("./server/quiz_ans.dat"))){
+            while(reader.hasNextLine()){
+                // read question information, format like Q/A
+                String line = reader.nextLine();
+                String[] format = line.split("/");
 
+                //format[0]: question, format[1]: answer
+                QAList.add(format);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return QAList;
     }
     
-   
-    
-
-
-
-
 }
-    
-
-
